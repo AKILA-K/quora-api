654a4cc8deb7dd708c56482571c061cdaddb4410
AKILA-K <akila.karthick89@gmail.com>
2019-08-17 08:28:57 +0530

    after changes done in answer and question controller

--- quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
@@ -1,8 +1,8 @@
 package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.UserDao;
-import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,4 +53,6 @@ public class SignupBusinessService {
         }
         return userAuthTokenEntity;
     }
-}
+
+    }
+
